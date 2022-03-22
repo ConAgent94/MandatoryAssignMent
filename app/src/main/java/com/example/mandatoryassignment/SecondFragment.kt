0e931a02d57927e40aaba9f0e61b8e6968953a87
Alexander Ask Bäcklund Bonacci <alex280q@edu.easj.dk>
2022-03-22 10:21:45 +0100

    Working

--- app/src/main/java/com/example/mandatoryassignment/SecondFragment.kt
+++ app/src/main/java/com/example/mandatoryassignment/SecondFragment.kt
@@ -32,8 +32,9 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.buttonBack.setOnClickListener {
+           /* findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)*/
+            findNavController().popBackStack() //Tilbage hvor man kom fra
         }
     }
 
